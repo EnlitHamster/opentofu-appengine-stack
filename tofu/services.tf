9e89f180ed7b31110d9f166f7a808bd848366fb9
Sandro Massa <sandro.massa@giga-storage.com>
2024-12-31 16:29:10 +0100

    [ADD] Service enabling module

---
+++ tofu/services.tf
@@ -0,0 +1,8 @@
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 17.1"
+
+  project_id = local.config.project_id
+  
+  activate_apis = tolist(local.config.apis)
+} 
