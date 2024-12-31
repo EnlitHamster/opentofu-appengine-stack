ab8819e80373a9f2ae59ebfc7f0910c53646c859
Sandro Massa <sandro.massa@giga-storage.com>
2024-12-31 12:21:40 +0100

    [RM] Service enabling

--- services.tf
+++
@@ -1,8 +0,0 @@
-module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 17.1"
-
-  project_id = local.config.project_id
-
-  activate_apis = tolist(local.config.apis)
-} 
