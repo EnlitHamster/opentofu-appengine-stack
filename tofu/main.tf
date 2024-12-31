59ca7cc020b5c27a1e6c0dcfb791079e6afc1519
Sandro Massa <sandro.massa@giga-storage.com>
2024-12-31 16:28:42 +0100

    [ADD] CI/CD user roles

--- tofu/main.tf
+++ tofu/main.tf
@@ -19,10 +19,28 @@ data "google_service_account" "opentofu_service_account" {
   account_id = local.config.service_account_name
 }
 
-resource "google_project_iam_member" "opentofu_storage_admin" {
+resource "google_project_iam_member" "opentofu_roles" {
   project = local.config.project_id
   member = "serviceAccount:${data.google_service_account.opentofu_service_account.email}"
 
   for_each = toset(local.config.service_account_roles)
+  role = each.value
+}
+
+resource "google_service_account" "github_actions_service_account" {
+  project = local.config.project_id
+  account_id = "github-actions-service-account"
+}
+
+resource "google_project_iam_member" "github_actions_service_account_roles" {
+  project = local.config.project_id
+  member = "serviceAccount:${google_service_account.github_actions_service_account.email}"
+
+  for_each = toset([
+    "roles/appengine.appAdmin",
+    "roles/storage.admin",
+    "roles/cloudbuild.builds.editor",
+    "roles/iam.serviceAccountUser"
+  ])
   role = each.value
 }
