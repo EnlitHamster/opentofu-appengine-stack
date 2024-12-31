267aa3d4f9d7e0efa48c76c79cf875d61f493c4b
Sandro Massa <sandro.massa@giga-storage.com>
2024-12-31 16:30:03 +0100

    [ADD] Output resources

---
+++ tofu/output.tf
@@ -0,0 +1,7 @@
+output "github_actions_service_account_email" {
+  value = google_service_account.github_actions_service_account.email
+}
+
+output "workload_identity_pool_provider_identifier" {
+    value = module.github-oidc.provider_name
+}
