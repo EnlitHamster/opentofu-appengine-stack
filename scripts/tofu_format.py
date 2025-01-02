c3d02f62784582576a1ef5cbe926d7db2da4e19a
Sandro Massa <sandro.massa@giga-storage.com>
2025-01-02 15:12:06 +0100

    [ADD] QoL formatting script

---
+++ scripts/tofu_format.py
@@ -0,0 +1,19 @@
+import subprocess
+import utils
+
+
+def run(config: utils.Config = utils.Config()) -> None:
+    command = (
+        'docker run'
+        ' --workdir=/srv/workspace'
+        f' --mount type=bind,source={config.tofu_dir},target=/srv/workspace'
+        ' --rm'
+        ' ghcr.io/opentofu/opentofu:latest'
+        ' fmt'
+    )
+
+    subprocess.run(command, shell=True, check=True)
+
+
+if __name__ == '__main__':
+    run()
