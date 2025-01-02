e55872e50a58a119652efe11f2dd8292d1ffae69
Sandro Massa <sandro.massa@giga-storage.com>
2025-01-02 15:32:48 +0100

    [ADD] Cloud Tasks queue

---
+++ tofu/queues.tf
@@ -0,0 +1,16 @@
+resource "google_cloud_tasks_queue" "task_queue" {
+  name     = "task-queue"
+  location = "europe-west1"
+
+  rate_limits {
+    max_concurrent_dispatches = 1
+    max_dispatches_per_second = 1
+  }
+
+  retry_config {
+    max_attempts       = 3
+    max_retry_duration = "3s"
+  }
+
+  depends_on = [ google_project_iam_member.opentofu_roles ]
+}
